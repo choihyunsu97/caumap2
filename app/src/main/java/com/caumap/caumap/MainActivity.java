cb7d84a317960f7160e25da4e0a77d672338e983
choihyunsu97 <geonlim2@cau.ac.kr>
2019-07-19 16:22:18 +0900

    Initial commit

---
+++ app/src/main/java/com/caumap/caumap/MainActivity.java
@@ -0,0 +1,18 @@
+package com.caumap.caumap;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onButton1Clicked(View v){
+        Toast.makeText(this,"눌림",Toast.LENGTH_LONG).show();
+    }
+}
